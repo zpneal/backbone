7a7bd73116ffd0c99b9ed1ea626ebd56bcb37ea5
Rachel Domagalski <domagal9@msu.edu>
2020-04-16 14:35:34 -0400

    add descriptions of functions in model.helpers

--- R/backbone.R
+++ R/backbone.R
@@ -11,15 +11,25 @@
 #'
 #' @details Some features of the package are:
 #' \itemize{
-#' \item '\code{\link{universal}}': returns a unipartite backbone matrix in which
-#' values are set to 1 if above the given upper parameter threshold,
-#' and set to -1 if below the given lower parameter threshold, and are 0 otherwise.
-#' \item '\code{\link{sdsm}}': computes the proportion of generated edges above or below the observed value using the stochastic degree sequence model. Once computed, use \code{\link{backbone.extract}} to return the backbone matrix for a given alpha value.
-#' \item '\code{\link{fdsm}}': computes the proportion of generated edges above or below the observed value using the fixed degree sequence model. Once computed, use \code{\link{backbone.extract}} to return the backbone matrix for a given alpha value.
-#' \item '\code{\link{hyperg}}': returns a binary or signed adjacency matrix
-#'  containing the backbone that retains only the significant edges.
-#' \item '\code{\link{backbone.extract}}': returns a positive or signed adjacency matrix
-#'  containing the backbone: only the significant edges.
+#' \item '\code{\link{universal}}': returns a backbone graph in which edge weights
+#'     are set to 1 if above the given upper parameter threshold,
+#'     and set to -1 if below the given lower parameter threshold, and are 0 otherwise.
+#' \item '\code{\link{sdsm}}': computes the probability of edge weights being above or below the observed edge weights in a bipartite projection using the stochastic degree sequence model.
+#'     Once computed, use \code{\link{backbone.extract}} to return the backbone matrix for a given alpha value.
+#' \item '\code{\link{hyperg}}': computes the probability of edge weights being above or below the observed edge weights in a bipartite projection using the hypergeometric model.
+#'     Once computed, use \code{\link{backbone.extract}} to return the backbone matrix for a given alpha value.
+#' \item '\code{\link{fdsm}}': computes the proportion of edge weights above or below the observed edge weights in a bipartite projection using the fixed degree sequence model.
+#'     Once computed, use \code{\link{backbone.extract}} to return the backbone matrix for a given alpha value.
+#' \item '\code{\link{backbone.extract}}': returns a backbone graph object that retains only the significant edges.
+#' }
+#'
+#' @details Additional functions that aid in the use of the above models are exported:
+#' \itemize{
+#' \item '\code{\link{class.convert}}': converts graph objects into adjacency matrices and vice versa
+#' \item '\code{\link{polytope}}': finds a matrix that maximizes the entropy function, used in \code{\link{sdsm}}.
+#' \item '\code{\link{curveball}}': generates a random 0/1 matrix with the same row and column sums as the input, used in \code{\link{sdsm}} and \code{\link{fdsm}}.
+#' \item '\code{\link{rna}}': computes the Poisson binomial distribution using the refined normal approximation, used in \code{\link{sdsm}}.
+#' \item '\code{\link{holm.bonferroni}}': helps with family-wise error rates for multiple hypothesis tests, used in \code{\link{backbone.extract}}.
 #' }
 #'
 #' @details For additional documentation and background on the package functions, see \code{browseVignettes("backbone")}.
