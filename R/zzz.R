6586f49dbe87e9dd48412aa05c1f5a58716cbfbb
Rachel Domagalski <domagal9@msu.edu>
2021-01-29 18:31:18 -0500

    add .onAttach startup message file

---
+++ R/zzz.R
@@ -0,0 +1,9 @@
+.onAttach <- function(lib,pkg) {
+  local_version <- utils::packageVersion("backbone")
+  packageStartupMessage(" ____  ")
+  packageStartupMessage("|  _ \\   backbone v",local_version)
+  packageStartupMessage("|#|_) |  Cite: Domagalski, R., Neal, Z. P., & Sagan, B. (2021). ")
+  packageStartupMessage("|# _ <   Backbone: An R package for extracting the backbone of bipartite projections. ")
+  packageStartupMessage("|#|_) |  PLoS ONE. https://doi.org/10.1371/journal.pone.0244363")
+  packageStartupMessage("|____/   For help: type vignette(\"backbone\"); email zpneal@msu.edu; github domagal9/backbone")
+}
