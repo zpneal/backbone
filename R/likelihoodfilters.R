5ffa3c27d09b70b316a74323ff1bf229cecf36d7
Rachel Domagalski <domagal9@msu.edu>
2021-06-02 20:57:02 -0400

    non-optimized glf, maybe working?

--- R/likelihoodfilters.R
+++ R/likelihoodfilters.R
@@ -83,12 +83,93 @@ mlf <- function(G){
 #' Global Likelihood Filter
 #'
 #' @param G
+#' @param m integer, number of edges to keep
+#' @param trials integer, number of matrices to try
 #'
 #' @return
 #' @export
 #'
 #' @examples
-glf <- function(G){
-  message("to be completed")
-}
+glf <- function(G, m, trials){
+  #### CAN DO WAY FASTER IF ONLY RECOMPUTE THE SINGLE EDGE
+  #### AKA ONLY COMPUTE THE CHANGE IN LLH
 
+  #### Define Parameters ####
+  get_llh <- function(sigma){
+    # sigma_ij = weight of ij edge = G
+
+    rs <- rowSums(sigma)
+    cs <- colSums(sigma)
+
+    #Nbar = sum_{i<j} sigma_ij
+    # sum_{i<j} = lower triangle minus diag
+    Nbar = sum(sigma)
+    #pij = (k_i *k_j)/(2T^2)
+    ## p = k_i*k_j/(2*t^2), k_i deg of i, k_j deg of j
+    t <- sum(sigma)/2
+    p = outer(rs,cs)/(2*t**2)
+
+    #llh <- log(factorial(Nbar))+sum_{i<j}[sigma_ij log p_ij - log(sigma_ij !)]
+    #1st term
+    #t1 <- sigma*(log(p))
+    #2nd term
+    #t2 <- log(factorial(sigma))
+    sigma_lower <- sigma
+    sigma_lower[upper.tri(sigma_lower)] <- NA
+    sigma_lower[diag(sigma_lower)] <- NA
+
+    p_lower <- p
+    p_lower[upper.tri(p_lower)] <- NA
+    p_lower[diag(p_lower)] <- NA
+
+    inner_sum <- log((p_lower**sigma_lower)/(factorial(sigma_lower)))
+    llh <- log(factorial(Nbar))+sum(na.omit(as.numeric(inner_sum)))
+    print(llh)
+    return(llh)
+  } #end get_llh
+
+  delta_llh <- function(old_llh, new_llh){
+    prob <- (1-exp(-(new_llh-old_llh)))
+  }
+
+  #### Monte Carlo ####
+
+  index <- matrix(1:length(G), nrow = nrow(G), ncol = ncol(G))
+  index[upper.tri(index)] <- NA
+  index[diag(index)] <- NA
+
+  starting_index <- sample(na.omit(as.numeric(index)), m, replace = FALSE)
+  starting_subgraph <- matrix(0, nrow = nrow(G), ncol = ncol(G))
+  starting_subgraph[starting_index] <- G[starting_index]
+
+  old_llh <- get_llh(sigma = starting_subgraph)
+  current_index <- starting_index
+  for (trial in 1:trials){
+    print(trial)
+
+    new_index <- index
+    new_index[current_index] <- NA
+    new_edge <- sample(na.omit(as.numeric(new_index)),1)
+    old_edge_index <- sample(length(current_index),1)
+    old_edge <- current_index[old_edge_index]
+    current_index <- replace(current_index, old_edge_index, new_edge)
+    new_subgraph <- matrix(0, nrow=nrow(G), ncol=ncol(G))
+    new_subgraph[current_index] <- G[current_index]
+    new_llh <- get_llh(sigma = new_subgraph)
+    if (new_llh < old_llh){
+      print("smaller")
+      old_llh <- new_llh
+    } else {
+      prob <- delta_llh(old_llh,new_llh)
+      # 0 back to original
+      # 1 keep
+      coin_flip <- sample(c(0,1), size = 1, prob = c(prob, 1-prob))
+      if (coin_flip == 0){
+        current_index <- replace(current_index, old_edge_index, old_edge)
+      }#end coin_flip
+    }#end else
+  }#end trials
+  bb <- matrix(0, nrow=nrow(G), ncol=ncol(G))
+  bb[current_index] <- G[current_index]
+  return(bb)
+}#end glf
