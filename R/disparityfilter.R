e6c08d0271c2a60cedd94fae160265959fee7cfe
Rachel Domagalski <domagal9@msu.edu>
2021-06-07 16:26:07 -0400

    fix comments, add stop condition, update summary

--- R/disparityfilter.R
+++ R/disparityfilter.R
@@ -4,8 +4,8 @@
 #'     Any rows and columns of the associated bipartite matrix that contain only zeros are automatically removed before computations.
 #'
 #' @return backbone, a list(positive, negative, summary). Here
-#'     `positive` is a matrix of probabilities of edge weights being equal to or above the observed value in the projection,
-#'     `summary` is a data frame summary of the inputted matrix and the model used including: model name, number of rows, skew of row sums, number of columns, skew of column sums, and running time.
+#'     `positive` is a matrix of probabilities of edge weights being equal to or above the observed value,
+#'     `summary` is a data frame summary of the inputted matrix and the model used including: class, model name, number of rows, number of columns, and running time.
 #' @references Serrano, M. Á., Boguñá, M., & Vespignani, A. (2009). Extracting the multiscale backbone of complex weighted networks. Proceedings of the National Academy of Sciences, 106(16), 6483–6488. \doi{10.1073/pnas.0808904106}
 #' @export
 #'
@@ -20,11 +20,12 @@ disparityfilter <- function(G){
   G <- convert$G
   class <- convert$summary[[1]]
   symmetric <- convert$summary[["symmetric"]]
+  if (convert$summary[["bipartite"]]==TRUE){stop("Graph must be unipartite.")}
+
+  #### Set Parameters ####
   strength <- rowSums(G)
   binary <- (G>0)+0
-  degree <- rowSums(binary) #equivalent to k
-
-  #Find all weight 0 edges
+  degree <- rowSums(binary)
   zeros <- G==0
 
   if (symmetric == TRUE){
@@ -35,6 +36,7 @@ disparityfilter <- function(G){
   }
 
   if (symmetric == FALSE){
+    ### Implies Directed ###
     outp <- G/strength
     outvalues <- (1-outp)^(degree-1)
     inp <- t(G)/(colSums(G))
@@ -43,8 +45,7 @@ disparityfilter <- function(G){
     negative <- 1-positive
   }
 
-  ### If edge weight was zero
-  ##     set to 1 in positive so not in backbone
+  ### If edge weight was zero, set to 1 in positive so edge not in backbone ###
   positive[zeros] <- 1
 
   ### Run Time ###
@@ -55,8 +56,8 @@ disparityfilter <- function(G){
   r <- rowSums(G)
   c <- colSums(G)
 
-  a <- c("Input Class", "Method", "Number of Rows", "Mean of Row Sums", "SD of Row Sums", "Skew of Row Sums", "Number of Columns", "Mean of Column Sums", "SD of Column Sums", "Skew of Column Sums", "Running Time (secs)")
-  b <- c(class[1], "Disparity Filter", dim(G)[1], round(mean(r),5), round(stats::sd(r),5), round((sum((r-mean(r))**3))/((length(r))*((stats::sd(r))**3)), 5), dim(G)[2], round(mean(c),5), round(stats::sd(c),5), round((sum((c-mean(c))**3))/((length(c))*((stats::sd(c))**3)), 5), as.numeric(total.time))
+  a <- c("Model", "Input Class", "Bipartite", "Symmetric", "Weighted", "Number of Rows", "Number of Columns", "Running Time (secs)")
+  b <- c("Disparity Filter", class[1], convert$summary$bipartite, convert$summary$symmetric, convert$summary$weighted, dim(G)[1], dim(G)[2], as.numeric(total.time))
   model.summary <- data.frame(a,b, row.names = 1)
   colnames(model.summary)<-"Model Summary"
 
