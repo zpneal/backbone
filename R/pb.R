9d261360ab9e70f2ba28850de5c080b13796292a
zpneal <zpneal@msu.edu>
2022-12-06 15:01:59 -0500

    Speedup and correct errors

--- R/pb.R
+++ R/pb.R
@@ -9,7 +9,7 @@
 #'
 #' @details
 #' The Refined Normal Approximation (RNA) offers a close approximation when `length(p)` is
-#'    large (Hong, 2013). This function is based on `ppoibin()` from the `poibin` package.
+#'    large (Hong, 2013).
 #'
 #' @return vector, length 2: The first value (if lower = TRUE) is the lower tail probability, the
 #'    probability of observing `k` or fewer successes when each trial has probability `p` of success.
@@ -24,23 +24,13 @@
 #' @examples
 #' pb(50,runif(100))
 pb <-function(k, p, lowertail=TRUE) {
-
-  #Compute parameters
-  mu <- sum(p)
-  sigma <- sqrt(sum(p*(1-p)))
-  gamma <- sum(p*(1-p)*(1-2*p))
-
-  #Lower tail p-value, if requested
-  if (lowertail) {
-    x <- (k+.5-mu)/sigma
-    lower <- stats::pnorm(x)+gamma/(6*sigma^3)*(1-x^2)*stats::dnorm(x)
-  } else {lower <- NA}
-
-  #Upper tail p-value
-  x <- (k-1+.5-mu)/sigma
-  upper <- 1 - (stats::pnorm(x)+gamma/(6*sigma^3)*(1-x^2)*stats::dnorm(x))
-
-  #Combine, truncate, return
+  pq <- p*(1-p)
+  sigma <- sqrt(sum(pq))
+  k <- (k+.5-sum(p))/sigma  #Continuity-corrected evaluation point
+  na <- stats::pnorm(k)  #Normal approximation
+  refined <- (sum(pq*(1-2*p)))/(6*sigma^3)*(1-k^2)*stats::dnorm(k)  #Refined normal correction
+  upper <- (1-na)-refined  #Upper-tail p-value
+  if (lowertail) {lower <- na+refined} else {lower <- NA}  #Lower-tail p-value, if requested
   prob <- c(lower,upper)
   prob[prob<0] <- 0
   prob[prob>1] <- 1
