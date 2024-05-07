4dc26f2aa4feae6bcaf97748047d8d45b2154c6b
zpneal <zpneal@msu.edu>
2024-05-07 14:17:08 -0400

    Add files via upload

--- R/sparsify.R
+++ R/sparsify.R
@@ -55,7 +55,7 @@
 #' @references {Neal, Z. P. (2022). backbone: An R Package to Extract Network Backbones. *PLOS ONE, 17*, e0269137. \doi{10.1371/journal.pone.0269137}}
 #'
 #' @examples
-#' U <- igraph::sbm.game(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
+#' U <- igraph::sample_sbm(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
 #' plot(U) #A hairball
 #' sparse <- sparsify(U, s = 0.6, escore = "jaccard", normalize = "rank",
 #' filter = "degree", narrative = TRUE)
@@ -314,7 +314,7 @@ sparsify.with.skeleton <- function(U, s, class = "original", narrative = FALSE) 
 #' @export
 #'
 #' @examples
-#' U <- igraph::sbm.game(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
+#' U <- igraph::sample_sbm(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
 #' plot(U) #A hairball
 #' sparse <- sparsify.with.gspar(U, s = 0.4, narrative = TRUE)
 #' plot(sparse) #Clearly visible communities
@@ -340,7 +340,7 @@ sparsify.with.gspar <- function(U, s, class = "original", narrative = FALSE) {
 #' @export
 #'
 #' @examples
-#' U <- igraph::sbm.game(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
+#' U <- igraph::sample_sbm(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
 #' plot(U) #A hairball
 #' sparse <- sparsify.with.lspar(U, s = 0.6, narrative = TRUE)
 #' plot(sparse) #Clearly visible communities
@@ -366,7 +366,7 @@ sparsify.with.lspar <- function(U, s, class = "original", narrative = FALSE) {
 #' @export
 #'
 #' @examples
-#' U <- igraph::sbm.game(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
+#' U <- igraph::sample_sbm(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
 #' plot(U) #A hairball
 #' sparse <- sparsify.with.simmelian(U, s = 0.5, narrative = TRUE)
 #' plot(sparse) #Clearly visible communities
@@ -392,7 +392,7 @@ sparsify.with.simmelian <- function(U, s, class = "original", narrative = FALSE)
 #' @export
 #'
 #' @examples
-#' U <- igraph::sbm.game(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
+#' U <- igraph::sample_sbm(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
 #' plot(U) #A hairball
 #' sparse <- sparsify.with.jaccard(U, s = 0.3, narrative = TRUE)
 #' plot(sparse) #Clearly visible communities
@@ -418,7 +418,7 @@ sparsify.with.jaccard <- function(U, s, class = "original", narrative = FALSE) {
 #' @export
 #'
 #' @examples
-#' U <- igraph::sbm.game(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
+#' U <- igraph::sample_sbm(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
 #' plot(U) #A hairball
 #' sparse <- sparsify.with.meetmin(U, s = 0.5, narrative = TRUE)
 #' plot(sparse) #Clearly visible communities
@@ -444,7 +444,7 @@ sparsify.with.meetmin <- function(U, s, class = "original", narrative = FALSE) {
 #' @export
 #'
 #' @examples
-#' U <- igraph::sbm.game(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
+#' U <- igraph::sample_sbm(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
 #' plot(U) #A hairball
 #' sparse <- sparsify.with.geometric(U, s = 0.25, narrative = TRUE)
 #' plot(sparse) #Clearly visible communities
@@ -470,7 +470,7 @@ sparsify.with.geometric <- function(U, s, class = "original", narrative = FALSE)
 #' @export
 #'
 #' @examples
-#' U <- igraph::sbm.game(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
+#' U <- igraph::sample_sbm(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
 #' plot(U) #A hairball
 #' sparse <- sparsify.with.hypergeometric(U, s = 0.3, narrative = TRUE)
 #' plot(sparse) #Clearly visible communities
@@ -522,7 +522,7 @@ sparsify.with.localdegree <- function(U, s, class = "original", narrative = FALS
 #' @export
 #'
 #' @examples
-#' U <- igraph::sbm.game(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
+#' U <- igraph::sample_sbm(60, matrix(c(.75,.25,.25,.25,.75,.25,.25,.25,.75),3,3), c(20,20,20))
 #' plot(U) #A hairball
 #' sparse <- sparsify.with.quadrilateral(U, s = 0.5, narrative = TRUE)
 #' plot(sparse) #Clearly visible communities in a connected graph
