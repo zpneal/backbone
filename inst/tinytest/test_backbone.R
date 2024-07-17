292164fb8a47658280aa82aa2b812a5312f09ba8
zpneal <zpneal@msu.edu>
2024-07-17 16:07:33 -0400

    Add files via upload

--- inst/tinytest/test_backbone.R
+++ inst/tinytest/test_backbone.R
@@ -9,8 +9,3 @@ M <- matrix(rbinom(100*1000,1,0.5),100,1000)
 test <- fastball(M)
 expect_equal(rowSums(test), rowSums(M), info = "fastball rows")
 expect_equal(colSums(test), colSums(M), info = "fastball columns")
-
-## PB (poisson-binomial)
-test <- pb(5, c(.123,.234,.345,.456,.567,.678,.789,.801,.911))
-expect_equal(test[1], 0.6773302, info = "pb lower")
-expect_equal(test[2], 0.6268476, info = "pb upper")
