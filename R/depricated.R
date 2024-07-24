3ad2bf010560be263b03a3c725e5f063e74593dd
zpneal <zpneal@msu.edu>
2024-07-24 09:22:21 -0400

    Add files via upload

---
+++ R/depricated.R
@@ -0,0 +1,18 @@
+#' Depricated function to extract SDSM backbone
+#' See backbone v2.1.4 for original documentation
+#' @noRd
+sdsm <- function(B, alpha = 0.05, missing.as.zero = FALSE, signed = FALSE, mtc = "none", narrative = FALSE, ...){
+  message("The sdsm() function is depricated in backbone v3.")
+  message("This command should mostly work for now, however use")
+  message("backbone_from_bipartite(model = \"sdsm\") in the future.")
+
+  return(
+  backbone_from_bipartite(B,
+                          model = "sdsm",
+                          alpha = alpha,
+                          signed = signed,
+                          mtc = mtc,
+                          missing_as_zero = missing.as.zero,
+                          narrative = narrative)
+  )
+}
