f8e235e303c77708413523d1f8eb351c3fa07ab9
zpneal <zpneal@msu.edu>
2024-07-25 09:53:53 -0400

    Add files via upload

--- R/depricated.R
+++ R/depricated.R
@@ -1,6 +1,6 @@
 #' Depricated function to extract SDSM backbone
 #'
-#' \code{sdsm()} was replaced by \code{backbone_from_bipartite()}.
+#' \code{sdsm()} was replaced by \code{backbone_from_bipartite(model="sdsm")}.
 #'
 #' @param B An unweighted bipartite network as a binary incidence matrix or a binary bipartite \code{\link{igraph}} object
 #' @param alpha real: significance level of hypothesis test(s)
@@ -24,5 +24,34 @@ sdsm <- function(B, alpha = 0.05, missing.as.zero = FALSE, signed = FALSE, mtc =
                           mtc = mtc,
                           missing_as_zero = missing.as.zero,
                           narrative = TRUE)
+  )
+}
+
+#' Depricated function to extract FIXEDROW backbone
+#'
+#' \code{fixedrow()} was replaced by \code{backbone_from_bipartite(model = "fixedrow")}.
+#'
+#' @param B An unweighted bipartite network as a binary incidence matrix or a binary bipartite \code{\link{igraph}} object
+#' @param alpha real: significance level of hypothesis test(s)
+#' @param signed boolean: return a signed backbone
+#' @param mtc string: type of Multiple Test Correction, either \code{"none"} or a method allowed by \code{\link{p.adjust}}.
+#' @param missing.as.zero boolean: treat missing edges as edges with zero weight and test them for significance
+#' @param narrative boolean: display suggested text & citations
+#'
+#' @details
+#' See backbone v2.1.4 for original documentation
+#'
+#' @export
+fixedrow <- function(B, alpha = 0.05, missing.as.zero = FALSE, signed = FALSE, mtc = "none", narrative = FALSE){
+  .Deprecated("backbone_from_bipartite(model = \"fixedrow\")")
+
+  return(
+    backbone_from_bipartite(B,
+                            model = "fixedrow",
+                            alpha = alpha,
+                            signed = signed,
+                            mtc = mtc,
+                            missing_as_zero = missing.as.zero,
+                            narrative = TRUE)
   )
 }
---
+++ R/fixedrow.R
@@ -0,0 +1,47 @@
+#' Compute edgewise p-values under the Fixed Row Model
+#'
+#' @param I a binary incidence matrix
+#' @param missing_as_zero boolean: should missing edges be treated as edges with zero weight and tested for significance
+#' @param signed boolean: TRUE for a signed backbone, FALSE for a binary backbone
+#'
+#' @return
+#' If `signed = FALSE` a list containing a matrix of upper-tail p-values.
+#'
+#' If `signed = TRUE` a list containing a matrix of lower-tail and upper-tail p-values
+#'
+#' @references package: {Neal, Z. P. (2022). backbone: An R Package to Extract Network Backbones. *PLOS ONE, 17*, e0269137. \doi{10.1371/journal.pone.0269137}}
+#' @references sdsm: {Neal, Z. P. (2014). The backbone of bipartite projections: Inferring relationships from co-authorship, co-sponsorship, co-attendance, and other co-behaviors. *Social Networks, 39*, 84-97. \doi{10.1016/j.socnet.2014.06.001}}
+#'
+#' @noRd
+.fixedrow <- function(I, missing_as_zero, signed){
+
+  P <- tcrossprod(I)  #Weighted bipartite projection
+
+  #### Prepare dyad list ####
+  #Although P is symmetric, this is a complete list of dyads; AFTER TESTING, TRY TO RE-WRITE WITH ONLY NECESSARY EDGES
+  df <- data.frame(as.vector(P))  #Dataframe of dyads
+  names(df)[names(df)=="as.vector.P."] <- "weight"
+
+  rs <- rowSums(I)  #Find row sums in bipartite (agent degrees)
+  df$row_sum_i <- rep(rs, times = nrow(I))  #Add rowsums to dataframe
+  df$row_sum_j <- rep(rs, each = nrow(I))
+  df$diff <- ncol(I)-df$row_sum_i  #Difference in total number of artifacts and i's degree
+
+  #### Compute p-values ####
+  df$upper <- stats::phyper(df$weight-1, df$row_sum_i, df$diff, df$row_sum_j, lower.tail=FALSE)
+  upper <- matrix(as.numeric(df$upper), nrow = nrow(I), ncol = nrow(I), dimnames = list(rownames(P),colnames(P)))
+
+  if (signed) {
+    df$lower <- stats::phyper(df$weight, df$row_sum_i, df$diff, df$row_sum_j, lower.tail = TRUE)
+    lower <- matrix(as.numeric(df$lower), nrow = nrow(I), ncol = nrow(I), dimnames = list(rownames(P),colnames(P)))
+  }
+
+  #### If missing edges should *not* be treated as having zero weight, remove p-value and do not consider for backbone ####
+  if (!missing_as_zero) {
+    upper[P == 0] <- NA
+    if (signed) {lower[P == 0] <- NA}
+  }
+
+  if (signed) {return(list(lower = lower, upper = upper))}
+  if (!signed) {return(list(upper = upper))}
+  }
