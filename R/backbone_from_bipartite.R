da9c3c1ac3cd65940fe536c5561ef2320e47cbde
zpneal <zpneal@msu.edu>
2024-08-12 11:09:31 -0400

    Update backbone_from_bipartite.R

--- R/backbone_from_bipartite.R
+++ R/backbone_from_bipartite.R
@@ -173,10 +173,7 @@ bipartite projection, cautiously consider using backbone_from_weighted() instead
   }
 
   if (methods::is(B,"igraph")) {
-    if (model=="sdsm_ec") {  #If there are structural values, remove them
-      E(B)$weight[which(E(B)$weight==10)] <- 0  #Make structural 0s ordinary 0
-      E(B)$weight[which(E(B)$weight==11)] <- 1  #Make structural 1s ordinary 1
-    }
+    if (model=="sdsm_ec") {B <- igraph::delete_edges(B, which(E(B)$weight==10))}  #If there are prohibited edges in an igraph object, remove them
     P <- igraph::bipartite_projection(B, which="false")  #Generate weighted projection, with any agent attributes
     igraph::E(P)$oldweight <- igraph::E(P)$weight  #Save old edge weights
     P <- igraph::delete_edge_attr(P, "weight")  #Delete weight attribute
