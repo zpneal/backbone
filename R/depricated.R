4297c85974bd962faf56e58d202f113e5ca474e0
zpneal <zpneal@msu.edu>
2024-08-28 14:26:26 -0400

    Add files via upload

--- R/depricated.R
+++ R/depricated.R
@@ -113,3 +113,32 @@ fixedfill <- function(B, alpha = 0.05, missing.as.zero = FALSE, signed = FALSE, 
                             narrative = TRUE)
   )
 }
+
+#' Depricated function to extract Fixed Degree Sequence Model (FDSM) backbone
+#'
+#' \code{fdsm()} was replaced by \code{backbone_from_bipartite(model = "fdsm")}.
+#'
+#' @param B An unweighted bipartite network as a binary incidence matrix or a binary bipartite \code{\link{igraph}} object
+#' @param alpha real: significance level of hypothesis test(s)
+#' @param signed boolean: return a signed backbone
+#' @param mtc string: type of Multiple Test Correction, either \code{"none"} or a method allowed by \code{\link{p.adjust}}.
+#' @param missing.as.zero boolean: treat missing edges as edges with zero weight and test them for significance
+#' @param narrative boolean: display suggested text & citations
+#'
+#' @details
+#' See backbone v2.1.4 for original documentation
+#'
+#' @export
+fdsm <- function(B, alpha = 0.05, missing.as.zero = FALSE, signed = FALSE, mtc = "none", narrative = FALSE){
+  .Deprecated("backbone_from_bipartite(model = \"fdsm\")")
+
+  return(
+    backbone_from_bipartite(B,
+                            model = "fdsm",
+                            alpha = alpha,
+                            signed = signed,
+                            mtc = mtc,
+                            missing_as_zero = missing.as.zero,
+                            narrative = TRUE)
+  )
+}
