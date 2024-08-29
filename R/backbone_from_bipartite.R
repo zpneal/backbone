233f72c7b7eff32db8db9bafffbac8601b67c040
zpneal <zpneal@msu.edu>
2024-08-29 16:25:47 -0400

    Add files via upload

--- R/backbone_from_bipartite.R
+++ R/backbone_from_bipartite.R
@@ -78,6 +78,7 @@ backbone_from_bipartite <- function(B,
   if (!is.logical(signed)) {stop("`signed` must be either TRUE or FALSE")}
   if (!(mtc %in% c("none", "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr"))) {stop("`mtc` must be one of: \"none\", \"holm\", \"hochberg\", \"hommel\", \"bonferroni\", \"BH\", \"BY\", or \"fdr\"")}
   if (!is.logical(missing_as_zero)) {stop("`missing_as_zero` must be either TRUE or FALSE")}
+  if (!is.logical(narrative)) {stop("`narrative` must be either TRUE or FALSE")}
   if (model=="fdsm" & !is.null(trials)) {  #If FDSM and `trials` is supplied, check it
     if (!is.numeric(trials)) {stop("`trials` must be a positive integer")}
     if (trials%%1!=0 | trials < 1) {stop("`trials` must be a positive integer")}
