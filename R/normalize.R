c9e15beb2c0335fd5cfbaac02f896b424101c9a0
zpneal <zpneal@msu.edu>
2025-08-13 08:38:49 -0400

    Add files via upload

---
+++ R/normalize.R
@@ -0,0 +1,48 @@
+#' Normalize edge scores
+#'
+#' @param G a weighted adjacency matrix
+#' @param normalize string: type of normalization
+#'
+#' @return A weighted adjacency matrix
+#'
+#' @noRd
+.normalize <- function(G, normalize) {
+  
+  #### Neighborhood rank, from Satuluri et al. (2011) ####
+  if (normalize == "rank" | normalize == "embeddedness") {
+    for (i in 1:nrow(G)) {  #For each row (i.e., from the perspective of each node)
+      x <- G[i,]  #Vector of values from this row
+      old <- sort(unique(x))  #Find unique values
+      new <- c((length(old)):1)  #Rank them 1 = highest, 2 = second highest, etc
+      if (min(old)==0) {new[which(new==max(new))] <- 0}  #If zero was one of the values, rank them as 0
+      x <- new[match(x, old)]  #Replace original values with corresponding ranks
+      G[i,] <- x  #Put ranks into row
+      }
+    }  
+  
+  #### Embeddedness, from Nick et al. (2013) ####
+  if (normalize == "embeddedness") {  #Scores will already be transformed as neighborhood ranks
+    scores <- matrix(0, nrow(G), ncol(G))  #Initialize matrix to hold embeddedness scores
+    for (row1 in 1:(nrow(G)-1)) {
+      for (row2 in (row1+1):nrow(G)) {  #Loop over each pair of rows
+        list1 <- G[row1,-c(row1,row2)]  #Vector of ranked edges for row1, excluding row1 and row2
+        list2 <- G[row2,-c(row1,row2)]  #Vector of ranked edges for row2, excluding row1 and row2
+        
+        #Find overlap between neighborhoods using non-parametric variant
+        k <- max(list1,list2)
+        if (k==0 | ((sum((list1>0 & list1<=k) & (list2>0 & list2<=k))) / (sum((list1>0 & list1<=k) | (list2>0 & list2<=k))))==0) {  #If jaccard for max(k) is zero, stop
+          scores[row1,row2] <- 0
+        } else {  #Otherwise, compute jaccard for each k, use maximum
+          j <- NULL
+          for (k in 1:max(list1,list2)) {j <- c(j, ((sum((list1>0 & list1<=k) & (list2>0 & list2<=k))) / (sum((list1>0 & list1<=k) | (list2>0 & list2<=k)))))}
+          scores[row1,row2] <- max(j)
+        }
+      }
+    }
+    G <- scores
+    G[lower.tri(G)] <- t(G)[lower.tri(G)]  #Fill in rest of matrix
+  }
+
+  return(G)
+
+}
