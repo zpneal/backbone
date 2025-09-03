ebd6ad27bc9b0c1a984e6d126a9880ecb2a138a9
zpneal <zpneal@msu.edu>
2025-09-03 09:50:17 -0400

    Add files via upload

--- R/backbone.R
+++ R/backbone.R
@@ -1,8 +1,12 @@
 #' backbone: Extracts the Backbone from Graphs
 #'
-#' @description Provides methods for extracting from an unweighted and sparse subgraph (i.e., a backbone)
-#'    that contains only the most "important" edges in a weighted bipartite projection, a non-projection
-#'    weighted network, or an unweighted network.
+#' @description Provides methods for extracting an unweighted and sparse subgraph (i.e., a backbone)
+#'    that contains only the most "important" edges from:
+#'    * a weighted network using [backbone_from_weighted()]
+#'    * a weighted bipartite projection using [backbone_from_bipartite()]
+#'    * an unweighted network using [backbone_from_unweighted()]
+#'
+#'    For a detailed illustration of these methods, please see the vignette using [vignette("backbone")].
 #'
 #' @references {Neal, Z. P. (2022). backbone: An R Package to Extract Network Backbones. *PLOS ONE, 17*, e0269137. \doi{10.1371/journal.pone.0269137}}
 #'
