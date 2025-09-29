92109f33f3a08c1bb98338e220a2c2a9fd998cb0
zpneal <zpneal@msu.edu>
2025-09-29 15:38:53 -0400

    Add files via upload

--- R/utils.R
+++ R/utils.R
@@ -14,24 +14,25 @@ print.backbone <- function(x, ...) {
   cat("\n=== SUMMARY OF BACKBONE OBJECT ===\n")
 
   #Source network
-  if ("bipartite" %in% names(x) & (methods::is(x$bipartite, "matrix") | methods::is(x$bipartite, "Matrix"))) {source <- paste0("ORIGINAL:   Unweighted bipartite: ", nrow(x$bipartite), " agents, ", ncol(x$bipartite), " artifacts, and ", sum(x$bipartite), " edges")}
-  if ("bipartite" %in% names(x) & (methods::is(x$bipartite, "igraph"))) {source <- paste0("ORIGINAL:   Unweighted bipartite: ", as.numeric(table(igraph::V(x$bipartite)$type)[1]), " agents, ", as.numeric(table(igraph::V(x$bipartite)$type)[2]), " artifacts, and ", igraph::gsize(x$bipartite), " edges")}
-  if ("weighted" %in% names(x) & (methods::is(x$weighted, "matrix") | methods::is(x$weighted, "Matrix"))) {if (isSymmetric(x$weighted)) {source <- paste0("ORIGINAL:   Weighted: ", nrow(x$weighted), " nodes and ", sum(x$weighted > 0)/2, " edges (weights: ", round(min(x$weighted[which(x$weighted!=0)]),2), " - ", round(max(x$weighted[which(x$weighted!=0)]),2), ")")}}
-  if ("weighted" %in% names(x) & (methods::is(x$weighted, "matrix") | methods::is(x$weighted, "Matrix"))) {if (!isSymmetric(x$weighted)) {source <- paste0("ORIGINAL:   Weighted: ", nrow(x$weighted), " nodes and ", sum(x$weighted > 0), " edges (weights: ", round(min(x[which(x$weighted!=0)]),2), " - ", round(max(x$weighted[which(x$weighted!=0)]),2), ")")}}
-  if ("weighted" %in% names(x) & (methods::is(x$weighted, "igraph"))) {source <- paste0("ORIGINAL:   Weighted: ", igraph::gorder(x$weighted), " nodes and ", igraph::gsize(x$weighted), " edges (weights: ", round(min(igraph::E(x$weighted)$weight),2), " - ", round(max(igraph::E(x$weighted)$weight),2), ")")}
-  if ("unweighted" %in% names(x) & (methods::is(x$unweighted, "matrix") | methods::is(x$unweighted, "Matrix"))) {source <- paste0("ORIGINAL:   Unweighted: ", nrow(x$unweighted), " nodes and ", sum(x$unweighted > 0), " edges")}
-  if ("unweighted" %in% names(x) & (methods::is(x$unweighted, "igraph"))) {source <- paste0("ORIGINAL:   Unweighted: ", igraph::gorder(x$unweighted), " nodes and ", igraph::gsize(x$unweighted), " edges")}
+  if ("bipartite" %in% names(x) & (methods::is(x$bipartite, "matrix") | methods::is(x$bipartite, "Matrix"))) {source <- paste0("ORIGINAL:   Bipartite with ", nrow(x$bipartite), " agents, ", ncol(x$bipartite), " artifacts, and ", sum(x$bipartite), " edges")}
+  if ("bipartite" %in% names(x) & (methods::is(x$bipartite, "igraph"))) {source <- paste0("ORIGINAL:   Bipartite with ", as.numeric(table(igraph::V(x$bipartite)$type)[1]), " agents, ", as.numeric(table(igraph::V(x$bipartite)$type)[2]), " artifacts, and ", igraph::gsize(x$bipartite), " edges")}
+  if ("weighted" %in% names(x) & (methods::is(x$weighted, "matrix") | methods::is(x$weighted, "Matrix"))) {if (isSymmetric(x$weighted)) {source <- paste0("ORIGINAL:   Weighted with ", nrow(x$weighted), " nodes and ", sum(x$weighted > 0)/2, " edges (weights: ", round(min(x$weighted[which(x$weighted!=0)]),2), " - ", round(max(x$weighted[which(x$weighted!=0)]),2), ")")}}
+  if ("weighted" %in% names(x) & (methods::is(x$weighted, "matrix") | methods::is(x$weighted, "Matrix"))) {if (!isSymmetric(x$weighted)) {source <- paste0("ORIGINAL:   Weighted with ", nrow(x$weighted), " nodes and ", sum(x$weighted > 0), " edges (weights: ", round(min(x[which(x$weighted!=0)]),2), " - ", round(max(x$weighted[which(x$weighted!=0)]),2), ")")}}
+  if ("weighted" %in% names(x) & (methods::is(x$weighted, "igraph"))) {source <- paste0("ORIGINAL:   Weighted with ", igraph::gorder(x$weighted), " nodes and ", igraph::gsize(x$weighted), " edges (weights: ", round(min(igraph::E(x$weighted)$weight),2), " - ", round(max(igraph::E(x$weighted)$weight),2), ")")}
+  if ("unweighted" %in% names(x) & (methods::is(x$unweighted, "matrix") | methods::is(x$unweighted, "Matrix"))) {source <- paste0("ORIGINAL:   Unweighted with ", nrow(x$unweighted), " nodes and ", sum(x$unweighted > 0), " edges")}
+  if ("unweighted" %in% names(x) & (methods::is(x$unweighted, "igraph"))) {source <- paste0("ORIGINAL:   Unweighted with ", igraph::gorder(x$unweighted), " nodes and ", igraph::gsize(x$unweighted), " edges")}
   cat(source,"\n\n")
 
   #Projection (if present)
-  if ("bipartite" %in% names(x) & (methods::is(x$bipartite, "matrix") | methods::is(x$bipartite, "Matrix"))) {projection <- paste0("PROJECTION: Weighted: ", nrow(x$projection), " nodes and ", sum(x$projection > 0)/2, " edges (weights: ", round(min(x$projection[which(x$projection!=0)]),2), " - ", round(max(x$projection[which(x$projection!=0)]),2), ")")}
-  if ("bipartite" %in% names(x) & (methods::is(x$bipartite, "igraph"))) {projection <- paste0("PROJECTION: Weighted: ", igraph::gorder(x$projection), " nodes and ", igraph::gsize(x$projection), " edges (weights: ", round(min(igraph::E(x$projection)$weight),2), " - ", round(max(igraph::E(x$projection)$weight),2), ")")}
+  if ("bipartite" %in% names(x) & (methods::is(x$bipartite, "matrix") | methods::is(x$bipartite, "Matrix"))) {projection <- paste0("PROJECTION: Weighted with ", nrow(x$projection), " nodes and ", sum(x$projection > 0)/2, " edges (weights: ", round(min(x$projection[which(x$projection!=0)]),2), " - ", round(max(x$projection[which(x$projection!=0)]),2), ")")}
+  if ("bipartite" %in% names(x) & (methods::is(x$bipartite, "igraph"))) {projection <- paste0("PROJECTION: Weighted with ", igraph::gorder(x$projection), " nodes and ", igraph::gsize(x$projection), " edges (weights: ", round(min(igraph::E(x$projection)$weight),2), " - ", round(max(igraph::E(x$projection)$weight),2), ")")}
   if ("bipartite" %in% names(x)) {cat(projection,"\n\n")}
 
   #Backbone
-  if (methods::is(x$backbone, "matrix") | methods::is(x$backbone, "Matrix")) {if (isSymmetric(x$backbone)) {backbone <- paste0("BACKBONE:   Unweighted: ", nrow(x$backbone), " nodes and ", sum(x$backbone!=0)/2, " edges")}}
-  if (methods::is(x$backbone, "matrix") | methods::is(x$backbone, "Matrix")) {if (!isSymmetric(x$backbone)) {backbone <- paste0("BACKBONE:   Unweighted: ", nrow(x$backbone), " nodes and ", sum(x$backbone!=0), " edges")}}
-  if (methods::is(x$backbone, "igraph")) {backbone <- paste0("BACKBONE:   Unweighted: ", igraph::gorder(x$backbone), " nodes and ", igraph::gsize(x$backbone), " edges")}
+  if ("lower" %in% names(x$pvalues)) {type <- "Signed with "} else {type <- "Unweighted with "}
+  if (methods::is(x$backbone, "matrix") | methods::is(x$backbone, "Matrix")) {if (isSymmetric(x$backbone)) {backbone <- paste0("BACKBONE:   ", type, nrow(x$backbone), " nodes and ", sum(x$backbone!=0)/2, " edges")}}
+  if (methods::is(x$backbone, "matrix") | methods::is(x$backbone, "Matrix")) {if (!isSymmetric(x$backbone)) {backbone <- paste0("BACKBONE:   ", type, nrow(x$backbone), " nodes and ", sum(x$backbone!=0), " edges")}}
+  if (methods::is(x$backbone, "igraph")) {backbone <- paste0("BACKBONE:   ", type, igraph::gorder(x$backbone), " nodes and ", igraph::gsize(x$backbone), " edges")}
   cat(backbone,"\n")
 
   #Model
@@ -56,13 +57,6 @@ print.backbone <- function(x, ...) {
   if (x$model == "quadrilateral") {model <- paste0("   Model:   Quadrilateral Simmelian with parameter = ", x$parameter)}
   if (x$model == "custom") {model <- paste0("   Model:   Custom unweighted backbone with parameter = ", x$parameter)}
   cat(model,"\n")
-
-  #Pvalues
-  if ("pvalues" %in% names(x)) {
-    p <- x$pvalues
-    if ("lower" %in% names(p)) {pvalues <- paste0("p-values:   Upper and lower tail values computed")} else {pvalues <- paste0("p-values:   Only upper tail values computed")}
-    cat(pvalues,"\n")
-  }
 
   #Call
   call <- paste0("    Call:   ", deparse(x$call, width.cutoff = 500L))
