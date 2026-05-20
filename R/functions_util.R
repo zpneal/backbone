ef37c689cb3a1a4e314799bb7541a624d73d63d0
zpneal <zpneal@msu.edu>
2026-05-20 13:06:56 -0400

    Update functions_util.R

--- R/functions_util.R
+++ R/functions_util.R
@@ -1,8 +1,8 @@
 .onAttach <- function(lib,pkg) {
   local_version <- utils::packageVersion("backbone")
   packageStartupMessage(" ____   backbone v",local_version)
-  packageStartupMessage("|  _ \\  Cite: Neal, Z. P., (2022). backbone: An R package to extract network backbones.")
-  packageStartupMessage("|#|_) |       PLOS One, 17, e0269137. https://doi.org/10.1371/journal.pone.0269137")
+  packageStartupMessage("|  _ \\  Cite: Neal, Z. P., (2026). backbone: An R package to extract network backbones.")
+  packageStartupMessage("|#|_) |       PLOS One, 21, e0349258. https://doi.org/10.1371/journal.pone.0349258")
   packageStartupMessage("|# _ < ")
   packageStartupMessage("|#|_) | Help: type vignette(\"backbone\"); email zpneal@msu.edu; github zpneal/backbone")
   packageStartupMessage("|____/  Beta: type devtools::install_github(\"zpneal/backbone\", ref = \"devel\")")
